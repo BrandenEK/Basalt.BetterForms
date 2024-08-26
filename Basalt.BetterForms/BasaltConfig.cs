ce9a80462fd6234d431ad19c7f1c627a9261cc02
unknown <brandenek03912@hotmail.com>
2024-08-26 17:04:15 -0400

    config: add basalt config object

---
+++ Basalt.BetterForms/BasaltConfig.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Basalt.BetterForms;
+
+/// <summary>
+/// A configuration object that stores window settings
+/// </summary>
+public class BasaltConfig
+{
+    internal WindowState Window { get; set; } = new();
+
+    internal class WindowState
+    {
+        public Point Location { get; set; }
+        public Size Size { get; set; }
+        public bool IsMaximized { get; set; } = true;
+    }
+}
